c661067ab8dd3ab1c118b05b5ebb676c5e3b5061
Mgaed75 <morgaane.durand@gmail.com>
2021-11-17 14:13:36 +0100

    Création du composant 'Button'

---
+++ src/App/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Button.css"
+
+/**
+ * Composant du button html
+ * @param {object} props props du composant
+ * @returns  component render
+ */
+const Button = (props) => {
+  return <button className="Button">Benjamin button</button>;
+};
