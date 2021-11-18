5bb8d5978f663eddb170f510a708ecdb1867abed
Mgaed75 <morgaane.durand@gmail.com>
2021-11-18 12:25:35 +0100

    Mise en oeuvre avec fragment

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Button from "./components/Button/Button";
 import FlexLayout from "./components/FlexLayout/FlexLayout";
+import Header from "./components/Header/Header";
 import MemeForm from "./components/MemeForm/MemeForm";
 import MemeViewer from "./components/MemeViewer/MemeViewer";
 class App extends React.Component {
@@ -31,51 +32,54 @@ class App extends React.Component {
    */
   render() {
     return (
-      <div className="App">
-        <FlexLayout>
-          <MemeViewer
-            meme={{
-              titre: "mon 1er même",
-              text: "stop la triche",
-              x: 390,
-              y: 530,
-              fontSize: 28,
-              color: "tomato",
-              fontWeigh: "900",
-              underline: true,
-              italic: true,
-              frameX: 0,
-              frameY: 0,
+      <>
+        <Header />
+        <div className="App">
+          <FlexLayout>
+            <MemeViewer
+              meme={{
+                titre: "mon 1er même",
+                text: "stop la triche",
+                x: 390,
+                y: 530,
+                fontSize: 28,
+                color: "tomato",
+                fontWeigh: "900",
+                underline: true,
+                italic: true,
+                frameX: 0,
+                frameY: 0,
+              }}
+              image={{
+                id: 0,
+                url: "img/meme1.jpg",
+                titre: "meme1",
+                h: 778,
+                w: 736,
+              }}
+            />
+            <MemeForm />
+          </FlexLayout>
+          {/* Exercice n°2*/}
+          counter:{this.state.counter}
+          <Button
+            text="Soustraire 1"
+            onButtonClicked={() => {
+              this.setState({ counter: this.state.counter - 1 });
+              console.log(this.state);
             }}
-            image={{
-              id: 0,
-              url: "img/meme1.jpg",
-              titre: "meme1",
-              h: 778,
-              w: 736,
-            }}
+            bgColor="tomato"
           />
-          <MemeForm/>
-        </FlexLayout>
-        {/* Exercice n°2*/}
-        counter:{this.state.counter}
-        <Button
-          text="Soustraire 1"
-          onButtonClicked={() => {
-            this.setState({ counter: this.state.counter - 1 });
-            console.log(this.state);
-          }}
-          bgColor="tomato"
-        />
-        <Button
-          text="Ajouter 1"
-          onButtonClicked={() => {
-            this.setState({ counter: this.state.counter + 1 });
-            console.log(this.state);
-          }}
-          bgColor="green"
-        />
-      </div>
+          <Button
+            text="Ajouter 1"
+            onButtonClicked={() => {
+              this.setState({ counter: this.state.counter + 1 });
+              console.log(this.state);
+            }}
+            bgColor="green"
+          />
+        </div>
+      </>
     );
   }
 }
