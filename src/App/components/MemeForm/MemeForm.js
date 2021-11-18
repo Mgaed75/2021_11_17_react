0a32bd4785955897159bccb0996bcd792e6ab08b
Mgaed75 <morgaane.durand@gmail.com>
2021-11-18 15:26:09 +0100

    Ajout des props et array.map pour images

--- src/App/components/MemeForm/MemeForm.js
+++ src/App/components/MemeForm/MemeForm.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import Button from "../Button/Button";
-import style from "./MemeForm.module.scss"
+import style from "./MemeForm.module.scss";
 
 const initialState = {};
 
@@ -21,31 +21,44 @@ function MemeForm(props) {
         <h1>Titre</h1>
         <input type="text" id="f_titre" placeholder="Un titre?" />
         <hr />
-
+        
         <h2>Image</h2>
         <select>
-          <option value="-1">NNN</option>
+          <option value="-1">Aucune</option>
+          {props.images.map((ele, i) => (
+            <option value={ele.id}>{ele.titre}</option>
+          ))}
         </select>
         <hr />
-
+        
         <h2>Texte</h2>
         <input type="text" placeholder="Formation ReactJS" />
         <div className={style.half}>
-          <label htmlFor="f_text_x">x:</label><input id="f_text_x" type="number" className={style.smallInput} min='0'/>c
-          <label htmlFor="f_text_y">y:</label><input id="f_text_y" type="number" className={style.smallInput}/>
+          <label htmlFor="f_text_x">x:</label>
+          <input
+            id="f_text_x"
+            type="number"
+            className={style.smallInput}
+            min="0"
+          />
+          <label htmlFor="f_text_y">y:</label>
+          <input id="f_text_y" type="number" className={style.smallInput} />
         </div>
         <hr />
-
-        <label htmlFor='f_text_color'>Color</label><input id='f_text_color' type="color" className={style.smallInput}/>
+        
+        <label htmlFor="f_text_color">Color</label>
+        <input id="f_text_color" type="color" className={style.smallInput} />
         <hr />
-
+       
         <h2>Décorations</h2>
         <h3>Font-Size</h3>
         <h3>Font-Weight</h3>
-        <input id="f_underline" type="checkbox" /><label htmlFor="f_underline">Underline</label>
-        /
-        <label htmlFor="f_italic">Italic</label><input id="f_italic" type="checkbox" />
-
+        
+        <input id="f_underline" type="checkbox" />
+        <label htmlFor="f_underline">Underline</label>/
+        <label htmlFor="f_italic">Italic</label>
+        <input id="f_italic" type="checkbox" />
+        
         <div className={style.half}>
           <Button type="reset" text="Clear" bgColor="tomato" />
           <Button
@@ -63,7 +76,11 @@ function MemeForm(props) {
   );
 }
 
-MemeForm.propTypes = {};
+MemeForm.propTypes = {
+  meme: PropTypes.object.isRequired,
+  images: PropTypes.array.isRequired,
+  onMemeChange: PropTypes.func.isRequired,
+};
 
 MemeForm.defaultProps = {};
 
