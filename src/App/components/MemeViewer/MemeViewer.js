b361f63b5ecb5d273ea5ce7b1095261425f2850c
Mgaed75 <morgaane.durand@gmail.com>
2021-11-18 09:47:36 +0100

    Ajout de props à la génération par défaut

--- src/App/components/MemeViewer/MemeViewer.js
+++ src/App/components/MemeViewer/MemeViewer.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './MemeViewer.module.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./MemeViewer.module.scss";
 
-const MemeViewer = () => (
+const MemeViewer = (props) => (
   <div className={styles.MemeViewer} data-testid="MemeViewer">
     MemeViewer Component
   </div>
