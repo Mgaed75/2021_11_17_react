fe2836a916961c0e07311874ad6da052f739e0e4
Mgaed75 <morgaane.durand@gmail.com>
2021-11-18 11:52:05 +0100

    Typage du composant

--- src/App/components/MemeViewer/MemeViewer.tsx
+++ src/App/components/MemeViewer/MemeViewer.tsx
@@ -7,28 +7,32 @@ interface I_Props {
   image?: I_Image;
 }
 
-const MemeViewer = (props: I_Props) => (
-  <svg
-    className={styles.MemeViewer}
-    data-testid="MemeViewer"
-    viewBox={`0 0 ${props.image ? props.image.w : 1000} ${
-      props.image ? props.image.h : 1000
-    }`}
-  >
-    {props.image && <image href={props.image.url} x="0" y="0" />}
-    <text
-      x={props.meme.x}
-      y={props.meme.y}
-      fill={props.meme.color}
-      fontSize={props.meme.fontSize}
-      textDecoration={props.meme.underline ? "underline" : "none"}
-      fontStyle={props.meme.italic ? "italic" : "none"}
-      fontWeight={props.meme.fontWeight}
+const MemeViewer: React.FC<I_Props> = (props: I_Props) => {
+  console.log(props);
+
+  return (
+    <svg
+      className={styles.MemeViewer}
+      data-testid="MemeViewer"
+      viewBox={`0 0 ${props.image ? props.image.w : 1000} ${
+        props.image ? props.image.h : 1000
+      }`}
     >
-      {props.meme.text}
-    </text>
-  </svg>
-);
+      {props.image && <image href={props.image.url} x="0" y="0" />}
+      <text
+        x={props.meme.x}
+        y={props.meme.y}
+        fill={props.meme.color}
+        fontSize={props.meme.fontSize}
+        textDecoration={props.meme.underline ? "underline" : "none"}
+        fontStyle={props.meme.italic ? "italic" : "none"}
+        fontWeight={props.meme.fontWeight}
+      >
+        {props.meme.text}
+      </text>
+    </svg>
+  );
+};
 
 // remplacer par le typage en ts
 /*
