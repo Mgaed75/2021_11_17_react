6d93edb32257796f946fb82ee159b8e1b60a46b9
Mgaed75 <morgaane.durand@gmail.com>
2021-11-18 12:11:03 +0100

    Ajout de FlexLayout

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import Button from "./components/Button/Button";
+import FlexLayout from "./components/FlexLayout/FlexLayout";
+import MemeForm from "./components/MemeForm/MemeForm";
 import MemeViewer from "./components/MemeViewer/MemeViewer";
 class App extends React.Component {
   counter = 0;
@@ -30,31 +32,32 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <MemeViewer
-          meme={{
-            titre: "mon 1er même",
-            text: "stop la triche",
-            x: 390,
-            y: 530,
-            fontSize: 28,
-            color: "tomato",
-            fontWeigh: "900",
-            underline: true,
-            italic: true,
-            frameX: 0,
-            frameY: 0,
-          }}
-          image={{
-            id: 0,
-            url: "img/meme1.jpg",
-            titre: "meme1",
-            h: 778,
-            w: 736,
-          }}
-        />
-
+        <FlexLayout>
+          <MemeViewer
+            meme={{
+              titre: "mon 1er même",
+              text: "stop la triche",
+              x: 390,
+              y: 530,
+              fontSize: 28,
+              color: "tomato",
+              fontWeigh: "900",
+              underline: true,
+              italic: true,
+              frameX: 0,
+              frameY: 0,
+            }}
+            image={{
+              id: 0,
+              url: "img/meme1.jpg",
+              titre: "meme1",
+              h: 778,
+              w: 736,
+            }}
+          />
+          <MemeForm/>
+        </FlexLayout>
         {/* Exercice n°2*/}
-
         counter:{this.state.counter}
         <Button
           text="Soustraire 1"
