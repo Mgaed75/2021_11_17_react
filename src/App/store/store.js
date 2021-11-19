915fc5f95b36945be9e2d3bb8136389cf4d2d9da
Mgaed75 <morgaane.durand@gmail.com>
2021-11-19 11:57:15 +0100

    Store combiné

--- src/App/store/store.js
+++ src/App/store/store.js
@@ -1,22 +1,72 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
+import { REST_ADR, REST_RESSOURCES } from "../config/config";
 
+//état initial des ressources
 export const ressourcesInitialStates = {
   memes: [],
   images: [],
 };
 
-/**
- * Enum d'actions publiques
- */
+// état initial du même courant
+export const currentInitialState = {
+    titre: "",
+    text: "",
+    x: 0,
+    y: 10,
+    fontSize: 10,
+    color: "#000000",
+    fontWeight: "500",
+    underline: false,
+    italic: false,
+    frameX: 0,
+    frameY: 0,
+    imageId: -1,
+  };
+  
+//Enum d'actions publiques pour les ressources courantes
 export const RESSOURCES_PUBLIC_ACTION = Object.freeze({
   REPLACE_MEMES_LIST: "REPLACE_MEMES_LIST",
   REPLACE_IMAGES_LIST: "REPLACE_IMAGES_LIST",
   ADD_MEME: "ADD_MEME",
 });
 
+// Enum d'actions publiques pour le même courant
+const CURRENT_PUBLIC_ACTIONS = Object.freeze({
+    UPDATE_CURRENT: "UPDATE_CURRENT",
+    CLEAR_MEME: "CLEAR_MEME",
+    SAVE_MEME: "SAVE_MEME",
+  });
+
+//Enum d'actions privées pour les ressources courantes
+const RESSOURCES_PRIVATE_ACTION = Object.freeze({
+    INIT: "INIT",
+    INIT_ARRAYS: "INIT_ARRAYS",
+  });
+
+/**
+ * Reducer pour les ressources
+ * @param {*} state
+ * @param {*} action
+ * @returns le nouveau state
+ */
 function ressourceReducer(state = ressourcesInitialStates, action) {
   console.log(action);
   switch (action.type) {
+    case RESSOURCES_PRIVATE_ACTION.INIT:
+        // initialisation
+      const pImages = fetch(`${REST_ADR}${REST_RESSOURCES.images}`).then((flux) => flux.json());
+      const pMemes = fetch(`${REST_ADR}${REST_RESSOURCES.memes}`).then((flux) => flux.json());
+      Promise.all([pImages, pMemes])
+        .then((array_array) => {
+            // déclenche un changement d'état lors de l'initialisation des tableaux
+            store.dispatch({
+                type: RESSOURCES_PRIVATE_ACTION.INIT_ARRAYS, values: array_array
+            })
+        });
+      return state;
+    case RESSOURCES_PRIVATE_ACTION.INIT_ARRAYS:
+        // initialisation des images et mêmes
+        return { ...state, memes: action.values[1], images: action.values[0] };
     case RESSOURCES_PUBLIC_ACTION.REPLACE_IMAGES_LIST:
       // copie le state et modifie les images par les valeurs passées en paramètres
       return { ...state, images: action.values };
@@ -31,12 +81,42 @@ function ressourceReducer(state = ressourcesInitialStates, action) {
   }
 }
 
-const store = createStore(ressourceReducer);
-// Le store s'abonne à mon reducer (écoute les changements)
+/**
+ * Reducer pour le même courant
+ * @param {*} state
+ * @param {*} action
+ * @returns
+ */
+const currentReducer = (state = currentInitialState, action) => {
+  switch (action.type) {
+    case CURRENT_PUBLIC_ACTIONS.UPDATE_CURRENT:
+      return { ...action.value };
+    case CURRENT_PUBLIC_ACTIONS.CLEAR_MEME:
+      return { ...currentInitialState };
+    default:
+      return state;
+  }
+};
+
+/**
+ * Création d'un store utilisant 2 reducers :
+ *  - currentReducer
+ *  - ressourceReducer
+ */
+const store = createStore(
+  combineReducers({ current: currentReducer, ressources: ressourceReducer })
+);
+// Le store s'abonne à mes reducers (écoute les changements)
 store.subscribe(() => {
   console.log(store.getState());
 });
 
+// déclenche le changement d'état à l'initialisation
+store.dispatch({
+    type: RESSOURCES_PRIVATE_ACTION.INIT,
+})
+
+/*
 // déclenche un changement d'état lors du remplacement d'images
 store.dispatch({
   type: RESSOURCES_PUBLIC_ACTION.REPLACE_IMAGES_LIST,
@@ -54,5 +134,6 @@ store.dispatch({
   type: RESSOURCES_PUBLIC_ACTION.ADD_MEME,
   value: [{ id: 20 }],
 });
+*/
 
 export default store;
