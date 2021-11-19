488ed1395d7eb09c73306aff5f8d2521e20108b3
Mgaed75 <morgaane.durand@gmail.com>
2021-11-19 16:51:46 +0100

    Ajout d'une Navbar

--- src/App/components/Navbar/Navbar.js
+++ src/App/components/Navbar/Navbar.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './Navbar.module.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./Navbar.module.scss";
+import { Navbar as NavBar, Container, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import "../../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Navbar = () => (
-  <div className={styles.Navbar} data-testid="Navbar">
-    Navbar Component
-  </div>
+  <NavBar bg="primary" variant="dark">
+    <Container>
+      <LinkContainer to="/">
+        <NavBar.Brand>Home</NavBar.Brand>
+      </LinkContainer>
+      <Nav className="me-auto">
+        <LinkContainer to="/">
+          <Nav.Link>Home</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="thumbnail">
+          <Nav.Link>Thumbnail</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/edit">
+          <Nav.Link>Edit</Nav.Link>
+        </LinkContainer>
+      </Nav>
+    </Container>
+  </NavBar>
 );
 
 Navbar.propTypes = {};
